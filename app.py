e4e224deda0c131dc1f64a8d3b48898d4dda4da1
Taha Shakir <tahashakir40@gmail.com>
2024-01-02 15:11:10 +0500

    Updated code

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-def add_two_variables(variable1, variable2):
-    result = variable1 + variable2
-    return result
-
-num1 = 5
-num2 = 7
-sum_result = add_two_variables(num1, num2)
-print(f"The sum of {num1} and {num2} is: {sum_result}")
+print("The is a test file. with a new update")
